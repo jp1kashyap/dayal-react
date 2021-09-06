3e85cf59a8cfbd3469283c90b5e64b63ea4c3be4
JP <jp1kashyap@gmail.com>
2021-09-06 09:43:30 +0530

    react lazy loading

--- src/HomeComponent/Home.js
+++ src/HomeComponent/Home.js
@@ -5,6 +5,7 @@ import Footer from "../Footer/Footer";
 import './home.css'
 import ListBox from "../ListBox/ListBox";
 import ContentBox from "../ListBox/ContextBox";
+import LazyLoad from 'react-lazy-load';
 function Home() {
     return(
         <>
@@ -13,22 +14,26 @@ function Home() {
             {/* carousel starts*/}
             <Carousel controls={false} >
                 <Carousel.Item>
+                    <LazyLoad>
                     <img
                         className="d-block w-100"
                         src="/images/slider1.jpg"
                         alt="First slide"
                     />
+                    </LazyLoad>
                     <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
                     </Carousel.Caption>
                 </Carousel.Item>
                 <Carousel.Item>
+                    <LazyLoad>
                     <img
                         className="d-block w-100"
                         src="/images/slider2.jpg"
                         alt="First slide"
                     />
+                    </LazyLoad>
                     <Carousel.Caption>
                         <h3>First slide label</h3>
                         <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
